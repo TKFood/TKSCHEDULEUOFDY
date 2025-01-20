41a081aa07f5f12076c2bf7a050fd2e8fb8ef586
at3158 <tk290@tkfood.com.tw>
2025-01-20 13:53:53 +0800

    FrmLogin

--- TKSCHEDULEUOFDY/Program.cs
+++ TKSCHEDULEUOFDY/Program.cs
@@ -16,7 +16,7 @@ namespace TKSCHEDULEUOFDY
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmLogin());
         }
     }
 }
